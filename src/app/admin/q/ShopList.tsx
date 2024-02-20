abc89a8f91f61412e85c924ed62811fac2367388
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-02-20 06:24:34 +0000

    create ShopList

---
+++ src/app/admin/q/ShopList.tsx
@@ -0,0 +1,21 @@
+import ListCard from '@/components/admin/ListCard'
+
+export default function ShopList() {
+  const res = []
+
+  for (let i = 0; i < 20; i++) {
+    res.push(
+      <ListCard
+        key={i}
+        shop={{
+          id: i,
+          title: 'Your mom ' + i,
+          time: new Date(),
+          description: 'Lorem ipsum',
+        }}
+      />
+    )
+  }
+
+  return <div className="space-y-2 mt-8">{res}</div>
+}
