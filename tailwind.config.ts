96ba972e322a69dcbac7eddaa3b9385a50db9e23
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-02-21 02:33:02 +0000

    daisyui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,28 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: '#EEA650',
-        secondary: '#FFDAC6',
+        // primary: '#EEA650',
+        // secondary: '#FFDAC6',
       },
     },
   },
   plugins: [daisyui],
   daisyui: {
-    themes: [],
+    themes: [
+      {
+        main: {
+          primary: '#eea650',
+          secondary: '#ffdac6',
+          accent: '#f16322',
+          neutral: '#191b16',
+          'base-100': '#f3f4f6',
+          info: '#0ea5e9',
+          success: '#22c55e',
+          warning: '#f87171',
+          error: '#dc2626',
+        },
+      },
+    ],
   },
 }
 export default config
