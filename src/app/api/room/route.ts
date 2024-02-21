4b84499f6b55bac4da5cc6208ba5f327e06754a8
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-02-21 04:36:49 +0000

    add get one room api

---
+++ src/app/api/room/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from 'next/server'
+import { getOneRoom } from '@/db/room'
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const roomId = searchParams.get('id')
+
+  if (!roomId) {
+    return Response.error()
+  }
+
+  const data = await getOneRoom({ rId: parseInt(roomId) })
+  return Response.json(data)
+}
--- src/db/room.ts
+++ src/db/room.ts
@@ -1,9 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient, Room } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function getAllRooms() {
   try {
     const rooms = await prisma.room.findMany()
+    return rooms
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+export async function getOneRoom(where: Prisma.RoomWhereInput) {
+  try {
+    const rooms = await prisma.room.findFirst({ where })
     return rooms
   } catch (err) {
     console.error(err)
