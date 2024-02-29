8f7a8f802e2c2783de5e45fa1f58e595a4e56568
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-02-29 14:58:06 +0000

    add basic search in user queue list

--- src/app/q/QueueList.tsx
+++ src/app/q/QueueList.tsx
@@ -1,18 +1,23 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import QueueCard from './QueueCard'
 import { Queue } from '@prisma/client'
 
 export default function QueueList({ queues }: { queues: Queue[] }) {
   const [keyword, setKeyword] = useState<string>('')
+  const [searchedList, setSearchedList] = useState<Queue[]>([])
+
+  useEffect(() => {
+    setSearchedList(queues.filter(i => i.room.title.includes(keyword)))
+  }, [keyword])
 
   return (
-    <main className="space-y-4 w-[600px]">
+    <main className="space-y-4 max-w-[600px]">
       <input
         type="text"
         placeholder="ค้นหาคิว..."
-        className="rounded-[50px] px-6 py-2 bg-[#d9d9d9]/50 w-full"
+        className="rounded-[50px] px-6 py-2 bg-[#d9d9d9]/50 w-full sticky top-16 backdrop-blur-2xl border-4 border-[#d9d9d9]-300 z-50"
         value={keyword}
         onChange={e => setKeyword(e.target.value)}
       />
@@ -20,13 +25,35 @@ export default function QueueList({ queues }: { queues: Queue[] }) {
       <br />
 
       <h1>รายการคิวที่กำลังต่อ</h1>
-      {queues.map(q =>
+      {searchedList.map(q =>
         !q.finished ? <QueueCard key={q.qId} queue={q} /> : null
       )}
       <br />
 
       <h1>รายการคิวที่เสร็จแล้ว</h1>
-      {queues.map(q =>
+      {searchedList.map(q =>
+        q.finished ? <QueueCard key={q.qId} queue={q} /> : null
+      )}
+      <br />
+      <h1>รายการคิวที่กำลังต่อ</h1>
+      {searchedList.map(q =>
+        !q.finished ? <QueueCard key={q.qId} queue={q} /> : null
+      )}
+      <br />
+
+      <h1>รายการคิวที่เสร็จแล้ว</h1>
+      {searchedList.map(q =>
+        q.finished ? <QueueCard key={q.qId} queue={q} /> : null
+      )}
+      <br />
+      <h1>รายการคิวที่กำลังต่อ</h1>
+      {searchedList.map(q =>
+        !q.finished ? <QueueCard key={q.qId} queue={q} /> : null
+      )}
+      <br />
+
+      <h1>รายการคิวที่เสร็จแล้ว</h1>
+      {searchedList.map(q =>
         q.finished ? <QueueCard key={q.qId} queue={q} /> : null
       )}
       <br />
