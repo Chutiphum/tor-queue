bc5ed47315a1139fc25d73b54863b75f58e5782d
Chutiphum <Chutiphum2545@gmail.com>
2024-03-07 01:30:37 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
-import { useRouter } from "next/router";
 
 export default function page() {
   const { data: session } = useSession();
@@ -13,24 +12,24 @@ export default function page() {
   // }
 
   return (
-    <div className="h-screen justify-center items-center">
-      <div className="bg-white p-5 shadow-xl">
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-5 shadow-xl rounded-xl">
         <div className="text-center">
           <div className="font-bold text-3xl">Login</div>
         </div>
-        <button onClick={() => signIn("google")} className="bg-white px-2 py-2 mt-5 border w-full rounded-xl flex justify-center items-center text-xl hover:scale-105 duration-300 text-[#002D74]">
+        <button onClick={() => signIn("google")} className="bg-white px-2 py-2 mt-5 border w-full rounded flex justify-center items-center text-xl hover:scale-105 duration-300">
           <Image src="/google-logo.png" width={30} height={30} alt="Google Logo" />
           <span className="p-2">Login with Google</span>
         </button>
+        {session && (
+          <button
+            onClick={() => signOut()}
+            className="bg-white px-2 py-2 mt-5 border w-full rounded flex justify-center items-center text-xl hover:scale-105 duration-300"
+          >
+            Logout
+          </button>
+        )}
       </div>
-      {session && (
-        <button
-          onClick={() => signOut()}
-          className="bg-white px-2 py-2 border w-full rounded-xl flex justify-center items-center text-xl hover:scale-105 duration-300 text-[#002D74]"
-        >
-          Logout
-        </button>
-      )}
     </div>
   );
 }
