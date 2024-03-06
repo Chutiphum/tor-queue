4a6b9dea47c91083f8a56e88f124335762a2283d
Chutiphum <Chutiphum2545@gmail.com>
2024-03-07 01:58:58 +0700

    2

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,11 +1,25 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const options: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-          })
-    ]
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        })
+    ],
+    // callbacks: {
+    //     async signIn(user) {
+    //         const { email } = user;
+    //         await prisma.user.create({
+    //             data: {
+                    
+    //             }
+    //         });
+    //         return true;
+    //     }
+    // }
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,33 +3,31 @@
 import React from "react";
 import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 export default function page() {
   const { data: session } = useSession();
-  // if (session) {
-  //   redirect("/");
-  // }
 
   return (
+
     <div className="flex justify-center items-center h-screen">
       <div className="bg-white p-5 shadow-xl rounded-xl">
         <div className="text-center">
           <div className="font-bold text-3xl">Login</div>
         </div>
+
         <button onClick={() => signIn("google")} className="bg-white px-2 py-2 mt-5 border w-full rounded flex justify-center items-center text-xl hover:scale-105 duration-300">
           <Image src="/google-logo.png" width={30} height={30} alt="Google Logo" />
           <span className="p-2">Login with Google</span>
         </button>
+
         {session && (
-          <button
-            onClick={() => signOut()}
-            className="bg-white px-2 py-2 mt-5 border w-full rounded flex justify-center items-center text-xl hover:scale-105 duration-300"
-          >
+          <button onClick={() => signOut()} className="bg-white px-2 py-2 mt-5 border w-full rounded flex justify-center items-center text-xl hover:scale-105 duration-300">
             Logout
           </button>
         )}
+
       </div>
+
     </div>
   );
 }
