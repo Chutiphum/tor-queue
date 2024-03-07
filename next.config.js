54d8ba1a6fd799ea5a102bbf721fba0d549aa0ad
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-03-07 17:42:59 +0000

    fix whatever caused next auth to not work

--- next.config.js
+++ next.config.js
@@ -23,14 +23,6 @@ const nextConfig = {
       }
     ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*'
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
