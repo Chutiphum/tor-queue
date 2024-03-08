31332c157aed6541dfdd5e7e37d945865f9e804c
Chutiphum <Chutiphum2545@gmail.com>
2024-03-08 16:11:20 +0700

    4

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,27 +1,33 @@
 'use client'
 
 import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import { Scanner } from '@yudiel/react-qr-scanner';
+// import { useRouter } from 'next/router'
 
 export default function Camera() {
     const [data, setData] = useState('No result');
+    // const router = useRouter()
+
+    // const handleBackClick = () => {
+    //     console.log("test");
+    //     router.push('/')
+    // };
 
     return (
         <div className="flex flex-col h-screen w-screen items-center justify-center">
-            <div className="w-60 bg-white drop-shadow-lg rounded-xl">
-                <QrReader
-                    onResult={(result: { text: React.SetStateAction<string>; }, error: any) => {
-                        if (result) {
-                            setData(result.text);
-                        }
-                        if (error) {
-                            console.error(error);
-                        }
-                    }}
-                    className="w-full h-full"
-                />
+            <div className='btn btn-outline btn-sm mb-4'>
+                <a href="/">Back</a>
             </div>
-            <a href={data} className="text-center m-4">{data}</a>
+            <div className="w-60 bg-neutral-700 drop-shadow-lg rounded-sm">
+                <div className='m-2'>
+                    <Scanner
+                        onResult={(text, result) => { setData(text); console.log(text, result); }}
+                        onError={(error) => console.log(error?.message)}
+
+                    />
+                </div>
+            </div>
+            <a href={data} className="text-center mt-4">{data}</a>
         </div>
     )
 };
