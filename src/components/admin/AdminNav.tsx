eba97aa21c89f1168670b683d0cf662d5d1c56e3
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-03-08 13:14:09 +0000

    signout button on admin page

--- src/components/admin/AdminNav.tsx
+++ src/components/admin/AdminNav.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter, usePathname } from 'next/navigation'
@@ -47,11 +47,17 @@ export default function AdminNav() {
           <AdminLink key={i.href} {...i} />
         ))}
         <Link
-          href="/"
+          href="/q"
+          className={`btn bg-red-500 hover:bg-red-600 text-white border-none w-full min-h-0 h-fit rounded-2xl py-2 text-lg font-normal text-start`}
+        >
+          กลับหน้าหลัก
+        </Link>
+        <button
+          onClick={() => signOut()}
           className={`btn bg-red-500 hover:bg-red-600 text-white border-none w-full min-h-0 h-fit rounded-2xl py-2 text-lg font-normal text-start`}
         >
           ลงชื่อออก
-        </Link>
+        </button>
       </div>
     </div>
   )
@@ -65,9 +71,9 @@ function AdminLink({ name, href }: { name: string; href: string }) {
       href={href}
       className={`btn ${
         href != '/' && pathname === href
-          ? 'bg-secondary shadow-none'
-          : 'bg-secondary/20'
-      } hover:bg-white border-none w-full min-h-0 h-fit rounded-2xl py-2 text-lg font-normal text-start`}
+          ? 'bg-secondary shadow-none hover:bg-white'
+          : 'bg-secondary/20 hover:bg-secondary/70'
+      } border-none w-full min-h-0 h-fit rounded-2xl py-2 text-lg font-normal text-start`}
     >
       {name}
     </Link>
