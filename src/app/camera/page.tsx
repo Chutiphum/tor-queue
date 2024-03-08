ec0f9d0da6d1222f61b6611fd677ace59270c790
Chutiphum <Chutiphum2545@gmail.com>
2024-03-08 16:56:11 +0700

    4

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,20 +1,41 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
-// import { useRouter } from 'next/router'
 
 export default function Camera() {
     const [data, setData] = useState('No result');
-    // const router = useRouter()
+    const [showModal, setShowModal] = useState(false);
 
-    // const handleBackClick = () => {
-    //     console.log("test");
-    //     router.push('/')
-    // };
+    useEffect(() => {
+        if (data !== 'No result') {
+            setShowModal(true);
+        }
+    }, [data]);
+
+    const handleOpenLink = () => {
+        window.open(data);
+        setShowModal(false);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     return (
         <div className="flex flex-col h-screen w-screen items-center justify-center">
+            {showModal && (
+                <div className="fixed w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+                    <div className="bg-white p-6 rounded-lg">
+                        <p className='text-center'>คุณต้องการเปิดลิงค์นี้ใช่หรือไม่ ?</p>
+                        <p className="text-center">{data}</p>
+                        <div className="flex justify-center mt-4">
+                            <button onClick={handleOpenLink} className="btn btn-primary mr-2 btn-sm">Open</button>
+                            <button onClick={handleCloseModal} className="btn btn-secondary btn-sm">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            )}
             <div className='btn btn-outline btn-sm mb-4'>
                 <a href="/">Back</a>
             </div>
@@ -23,7 +44,7 @@ export default function Camera() {
                     <Scanner
                         onResult={(text, result) => { setData(text); console.log(text, result); }}
                         onError={(error) => console.log(error?.message)}
-
+                        components={{ audio: true }}
                     />
                 </div>
             </div>
