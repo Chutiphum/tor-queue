970b07fcf15fa72f40d1bfc6feec94c6d1d8a2e6
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-03-10 09:56:28 +0000

    .

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import React, { useEffect, useState } from 'react'
 import Main from './_components/main'
-import { getAllUsers } from '@/db/user';
+import axios from 'axios'
 
-export default async function Page() {
+export default function Page() {
+    const [data, setData] = useState([])
 
-    const data = await getAllUsers()
+    axios.get(process.env.NEXT_PUBLIC_SERVER_URL + '/api/users').then(res => {
+        setData(res.data)
+    })
+
+    if (!data) {
+        return <p>loading...</p>
+    }
 
     return (
         <div>
